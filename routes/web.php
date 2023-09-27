7e443e605b151fc35da779126fb3500e1d614695
HarmanSharma113 <113177945+HarmanSharma113@users.noreply.github.com>
2023-09-27 14:34:34 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,25 @@
 
 use App\Todo;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\TodosController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about', 'AboutController@index');
-Route::get('todos', 'TodosController@index');
-Route::get('todos/{todoId}', 'TodosController@show');
-Route::get('new-todo', 'TodosController@create');
-Route::post('store-todo', 'TodosController@store');
-Route::get('todos/{todoId}/edit','TodosController@edit');
-Route::post('todos/{todoId}/update','TodosController@update');
-Route::get('todos/{todoId}/delete','TodosController@destroy');
-Route::get('todos/{todoId}/complete','TodosController@complete');
+Route::get('about', [AboutController::class,'index']);
 
+Route::get('todos', 'TodosController@index');
+Route::get('todos',[TodosController::class,'index'] );
+Route::get('todos/{todoId}',[TodosController::class,'show']);
+Route::get('new-todo', [TodosController::class,'create']);
+Route::post('store-todo', [TodosController::class,'store']);
+Route::get('todos/{todoId}/edit',[TodosController::class,'edit']);
+Route::post('todos/{todoId}/update',[TodosController::class,'update']);
+Route::get('todos/{todoId}/delete',[TodosController::class,'destroy']);
+Route::get('todos/{todoId}/complete',[TodosController::class,'complete']);
+// passing data to the view 
 Route::get('completed',function(){
     return view('completed')->with('todos',Todo::all());
 });
